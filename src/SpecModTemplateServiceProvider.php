5ac294e047e070a54c1d81143d8f566c7b143486
mohammed-elmasry <mohammed.mahmoud.business@gmail.com>
2020-09-30 22:07:54 +0200

    clean up the service provider of boilerplate code

--- src/SpecModTemplateServiceProvider.php
+++ src/SpecModTemplateServiceProvider.php
@@ -14,37 +14,7 @@ class SpecModTemplateServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/SpecModTemplate.php', 'spec_mod_template');
-        $this->publishThings();
-        // $this->loadViewsFrom(__DIR__.'/resources/views', 'spec_mod_template');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->registerRoutes();
-    }
 
-    /**
-     * Register the package routes.
-     *
-     * @return void
-     */
-    private function registerRoutes()
-    {
-        Route::group($this->routeConfiguration(), function () {
-            $this->loadRoutesFrom(__DIR__ . '/Http/routes.php');
-        });
-    }
-
-    /**
-    * Get the Blogg route group configuration array.
-    *
-    * @return array
-    */
-    private function routeConfiguration()
-    {
-        return [
-            'namespace'  => "Alexunisoft\SpecModTemplate\Http\Controllers",
-            'middleware' => 'api',
-            'prefix'     => 'api'
-        ];
     }
 
     /**
@@ -54,17 +24,6 @@ class SpecModTemplateServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Register facade
-        $this->app->singleton('spec_mod_template', function () {
-            return new SpecModTemplate;
-        });
-    }
 
-    public function publishThings(){
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../config/SpecModTemplate.php' => config_path('SpecModTemplate.php'),
-            ], 'config');
-        }
     }
 }
